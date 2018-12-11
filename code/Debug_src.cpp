9927468dd2c6d88a2862b83108142faff9bc2fef
nathias2004 <2015csb1037@iitrpr.ac.in>
2018-12-12 04:02:28 +0530

    Insert and Delete succesfully checked and executed

--- code/Debug_src.cpp
+++ code/Debug_src.cpp
@@ -1,4 +1,3 @@
-
 #include<iostream>
 #include<bits/stdc++.h>
 #include<boost/functional/hash.hpp>
@@ -1194,167 +1193,54 @@ void Delete_node(int Old_Node,int policy){
 
 
 int main(){
-	//initialise and define page and secondary memory and primary memory
-	//read the data from the file
-	string pageno1,pageno2;
-	int number = 1;
-	pageno1 = "page" + to_string(number) + ".txt";
+	
 	ifstream infile;
-	ofstream outfile1;
-	ofstream outfile2;
-	infile.open("dataset_btp.txt");
-	int count=0;
-	unordered_map<int,vector<int>>U_successor;
-	unordered_map<int,vector<int>>U_predecessor;
-	unordered_map<int,int>U_Nodes;
-	vector<int>Nodes;
+	infile.open("BPLUS.txt");
+	int count = 0;
 	if(infile.is_open()){
 		while(infile){
 			string s;
 		    if (!getline( infile, s )) break;
+		    if(count == 0){
+		    	count++;
+		    	continue;
+		    }
 		    istringstream ss( s );
 		    vector <string> record;
 		    while (ss)
 		    {
 		      string s;
-		      if (!getline( ss, s, ',' )) break;
+		      if (!getline( ss, s, ' ' )) break;
 		      record.push_back( s );
 		    }
-
-			if(count == 0){
-				U_successor[string_to_int(record[1])].push_back(string_to_int(record[2]));
-				U_predecessor[string_to_int(record[2])].push_back(string_to_int(record[1]));
-				if(U_Nodes[string_to_int(record[1])] == 0){
-					U_Nodes[string_to_int(record[1])] = 1;
-					Nodes.push_back(string_to_int(record[1]));
-				}
-
-				if(U_Nodes[string_to_int(record[2])] == 0){
-					U_Nodes[string_to_int(record[2])] = 1;
-					Nodes.push_back(string_to_int(record[2]));
-				}
-
-			}
-			count++;
-			if(count == 10080){
-				count = 0;
-			}
+		    Bplus[string_to_int(record[0])] = string_to_int(record[1]);
 		}
 	}
-	else{
-		cout<<"File not opened"<<endl;
-	}
-
-	infile.close();
-	cout<<Nodes.size()<<endl;
-	count = 0;
-	int p_no = 1;
-	for(int i=0;i<Nodes.size();i++){
-		Bplus[Nodes[i]] = p_no;
-		p_no++;
-	}
-	
-	count = 0;
-	infile.open("/home/sainath/Desktop/dataset_btp.txt");
-	if(infile.is_open()){
-		while(infile){
-			string s;
-		    if (!getline( infile, s )) break;
-		    istringstream ss( s );
-		    vector <string> record;
-		    while (ss)
-		    {
-		      string s;
-		      if (!getline( ss, s, ',' )) break;
-		      record.push_back( s );
-		    }
-
-			if(count == 0){
-				pageno1 = "page_"+to_string(Bplus[string_to_int(record[1])])+".txt";
-				outfile1.open(pageno1,ios_base::app);
-				outfile1<<"\ns "<<record[1]<<" "<<record[2]<<" "<<record[4]<<" ";
-			}
-			else{
-				outfile1<<record[4]<<" ";
-			}
-			count++;
-			if(count == 10080){
-				count = 0;
-				outfile1.close();
-			}
-		}
-	}
-
 	infile.close();
 
-	infile.open("/home/sainath/Desktop/dataset_btp.txt");
+	infile.open("Record_size.txt");
+	count = 0;
 	if(infile.is_open()){
 		while(infile){
 			string s;
 		    if (!getline( infile, s )) break;
+		    if(count == 0){
+		    	count++;
+		    	continue;
+		    }
 		    istringstream ss( s );
 		    vector <string> record;
 		    while (ss)
 		    {
 		      string s;
-		      if (!getline( ss, s, ',' )) break;
+		      if (!getline( ss, s, ' ' )) break;
 		      record.push_back( s );
 		    }
-
-			if(count == 0){
-				pageno2 = "page_"+to_string(Bplus[string_to_int(record[2])])+".txt";
-				outfile2.open(pageno2,ios_base::app);
-				outfile2<<"\np "<<record[2]<<" "<<record[1]<<" "<<record[4]<<" ";
-			}
-			else{
-				outfile2<<record[4]<<" ";
-			}
-			count++;
-			if(count == 10080){
-				count = 0;
-				outfile2.close();
-			}
+		    RecordSizes[string_to_int(record[0])] = string_to_int(record[1]);
 		}
 	}
+	infile.close();
 
-
-//File Size collection
-	for(int i=0;i<Nodes.size();i++){
-		streampos begin,end;
-		string FileName ="page_"+to_string(Bplus[Nodes[i]])+".txt";
-  		ifstream myfile (FileName, ios::binary);
-  		begin = myfile.tellg();
-  		myfile.seekg (0, ios::end);
-  		end = myfile.tellg();
-  		myfile.close();
-  		int A;
-  		A = end-begin;
-		RecordSizes[Nodes[i]] = A;
-	}
-
-
-
-
-	//Process and arrange the data 
-	//define the record and page 
-	//create the graph(static create) and arrange the nodes into the pages
-	
-	cluster_nodes_into_pages(Nodes,(PageSize)/2,0);
-
-	//Now in Bplus we know which record is in which page and each record contains all its successors and predecessors and the clustering of page is done
-
-	outfile1.open("BPLUS.txt",ios_base::app);
-	for (auto i : Bplus) 
-        outfile1<<i.first<<" "<< i.second<<endl;
-
-    outfile1.close();
-
-    outfile1.open("Record_size.txt",ios_base::app);
-    for(auto i:RecordSizes)
-    	outfile1<<i.first<<" "<<i.second<<endl;
-
-	cout<<"The new page is: "<<New_PageNo<<endl;
-	//Now the quering starts
 
 	while(1){
 		int flag1;
